cb4877902621669f4d8bdc1d056bbe0399fc6a4b
DraculK <kayranvieira456@hotmail.com>
2021-01-14 22:42:12 -0300

    Desafio 1 completo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+ const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,70 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  //Lista todos os repositórios criados
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  //Campos que a rota deve receber
+  const { title, url, techs } = request.body;
+  //Forma na qual o repositório vai ser armazenado
+  const repository = { id:uuid(), title, url, techs, likes:0. };
+  //Armazena o último repositório criado no array de repositórios
+  repositories.push(repository);
+  //Retorna o último repositório criado
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  //Recebe o id do projeto que queremos alterar
+  const { id } = request.params;
+  //Recebe os parâmetros que podemos alterar
+  const { title, url, techs } = request.body;
+  //Busca o id do repositório para que possamos alterar os dados
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  //Caso não ache o id de algum repositório retorna o erro abaixo
+  if (repositoryIndex < 0 ){
+    return response.status(400).json({ erro: "Repository not found." })
+  }
+  //Caso ache o id de algum repositório, nós criamos um novo projeto atualizando os dados escolhidos
+  const repository ={
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  }
+  //Atualiza o projeto pelo id que encontramos
+  repositories[repositoryIndex] = repository;
+  //Retorna o repositório atualizado
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  //Mesma ideia do update
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  if(repositoryIndex < 0){
+    return response.status(400).json({ erro: "Repository not found." });
+  }else{
+    //Remove o repositório pelo o índice
+    repositories.splice(repositoryIndex,1);
+    //Retorna o status 204, caso o repositório seja deletado com sucesso
+    return response.status(204).send();
+  }
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id);
+  if(repositoryIndex<0){
+    return response.status(400).json({ erro: "Repository not found."})
+  }else{
+    repositories[repositoryIndex].likes += 1;
+    return response.json( repositories[repositoryIndex] );
+  }
 });
 
 module.exports = app;
